e4217b20e0826a80c379273ed6042400ec63f645
João Costa <joaovitorcostaoliveira@gmail.com>
2024-02-25 16:47:40 -0300

    ler linha

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
-#!/usr/bin/env node
-
-const yargs = require("yargs");
-const {argv} = yargs(process.argv);
-
-const pokemon = async (pokemonName) => {
-    const response = await fetch(
-        `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
-    );
-    const pokemonJson = await response.json();
-    console.log(pokemonJson)
-}
-console.
-pokemon(argv.pokemon);
-
-const printFiveMoves = () => {
-    const moves = pokemon.moves.map(({ move }) => move.name);
-       console.log(moves.slice(0, 5));
-}
-
-printFiveMoves(argv.pokemon);
+const readline = require('node:readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  readline.question(`What's your name?`, name => {
+    console.log(`Hi ${name}!`);
+    readline.close();
+  });
